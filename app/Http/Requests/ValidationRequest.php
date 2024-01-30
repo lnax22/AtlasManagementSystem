ffa8bece847c85ef8401ca730efb680e21996a47
なな <nk.duffy00kara7@gmail.com>
2024-01-30 18:33:50 +0900

    ユーザーの新規登録画面にバリデーションを追加(生年月日訂正)

--- app/Http/Requests/ValidationRequest.php
+++ app/Http/Requests/ValidationRequest.php
@@ -33,12 +33,26 @@ class ValidationRequest extends FormRequest
             'under_name_kana' =>'required|regex:/\A[ァ-ヴー]+\z/u|string|max:30',
             'mail_address' =>'required|email:rfc,dns|unique:users,mail_address|max:100',
             'sex' =>'required|in:1,2,3',
+            'old' => 'date',
             'old_year' =>'required_with:old_month,old_day|after_or_equal:2000-01-01|date_format:Y',
             'old_month'=>'required_with:old_year,old_day|after_or_equal:2000-01-01|date_format:m',
             'old_day'=>'required_with:old_month,old_year|after_or_equal:2000-01-01|date_format:d',
             'role'=>'required|in:1,2,3,4',
             'password'=>'required|min:8|max:30|confirmed',
         ];
+    }
+
+    public function getValidatorInstance()
+    {
+        if ($this->input('old_day') && $this->input('old_month') && $this->input('old_year'))
+        {
+            $birthDate = implode('-', $this->only(['old_year', 'old_month', 'old_day']));
+            $this->merge([
+                'old' => $oldDate,
+            ]);
+        }
+
+        return parent::getValidatorInstance();
     }
 
 }
